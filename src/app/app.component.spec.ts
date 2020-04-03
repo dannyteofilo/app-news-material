75db89a593bb89ca8d1ebaf0666e2dae516a3538
Teofilo <dteofilo@ids.com.mx>
2020-04-03 09:41:07 -0600

    feat: sidebar, header and main created

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-news-v2'`, () => {
+  it(`should have as title 'app-news-material'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app-news-v2');
+    expect(app.title).toEqual('app-news-material');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app-news-v2!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app-news-material!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DateComponent } from './components/date/date.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    DateComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [SidebarComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
