178789ff23d1853cee01c5013b8f5344fcd975f7
Teofilo <dteofilo@ids.com.mx>
2020-04-09 09:18:23 -0500

    fix: restructuring project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { APP_ROUTES } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { DateComponent } from './components/date/date.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    DateComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTES,
+    PagesModule
   ],
-  exports: [SidebarComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,16 @@
+import { Routes, RouterModule } from "@angular/router";
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const pagesRoutes: Routes = [
+    {
+        path: '', component: PagesComponent,
+        children: [
+            { path: '', component: DashboardComponent },
+            // { path: 'newsapi', component: NewsapiComponent }
+        ]
+
+    }
+];
+
+export const PAGES_ROUTES = RouterModule.forChild(pagesRoutes);
